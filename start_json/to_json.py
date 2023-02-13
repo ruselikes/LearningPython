dbe3b80d0118079e706b1922c7c0359e3d322a9e
ruselikes <vipsteam2002@mail.ru>
2023-02-13 21:07:47 +0300

    TestBubbleSort + Correct to_json project

--- start_json/to_json.py
+++ start_json/to_json.py
@@ -1,18 +1,24 @@
 import json,functools
+# def to_json(func):
+#     @functools.wraps(func)
+#     def wrapper(*args,**kwargs):
+#         with open('test.json', 'w') as file:
+#             data = json.dumps(func(), ensure_ascii=False)
+#             file.write(data)
+#             # json.dump(func(),file,indent=4)#так тоже правильно
+#             return data
+#     return wrapper
+
 def to_json(func):
     @functools.wraps(func)
-    def wrapper():
-        with open('test.json', 'w') as file:
-            data = json.dumps(func(), ensure_ascii=False, indent=4)
-            file.write(data)
-            # json.dump(func(),file,indent=4)#так тоже правильно
-            return data
-    return wrapper
-
-
+    def wrapped(*args, **kwargs):
+        not_parsed_result = func(*args, **kwargs)
+        return json.dumps(not_parsed_result)
+    return wrapped
 # @py_to_json
 @to_json
 def get_data():
     return {'data': 42}
+
 
 get_data()
